f150bf1a47c1fde3c37d32c9cc0cf87e7c11dd5a
Max Harlynking <mharlynk@andrew.cmu.edu>
2016-03-30 02:36:21 -0400

    fixed scrollTo

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -35,4 +35,44 @@ jQuery(document).ready(function(){
     });
   }
   
+  $.fn.scrollTo = function( target, options, callback ){
+  if(typeof options == 'function' && arguments.length == 2){ callback = options; options = target; }
+  var settings = $.extend({
+    scrollTarget  : target,
+    offsetTop     : 50,
+    duration      : 500,
+    easing        : 'swing'
+  }, options);
+  return this.each(function(){
+    var scrollPane = $(this);
+    var scrollTarget = (typeof settings.scrollTarget == "number") ? settings.scrollTarget : $(settings.scrollTarget);
+    var scrollY = (typeof scrollTarget == "number") ? scrollTarget : scrollTarget.offset().top + scrollPane.scrollTop() - parseInt(settings.offsetTop);
+    scrollPane.animate({scrollTop : scrollY }, parseInt(settings.duration), settings.easing, function(){
+      if (typeof callback == 'function') { callback.call(this); }
+    });
+  });
+}
+  
+  $('#toAbout').click(function(){
+    $("header .menu-icon").toggleClass('rotated');
+    $(".menu").slideToggle(400);
+    $('#about').ScrollTo();
+  });
+  
+  $('#toGallery').click(function(){
+    $("header .menu-icon").toggleClass('rotated');
+    $(".menu").slideToggle(400);
+    $('#glry').ScrollTo();
+  });
+  
+  $('#toContact').click(function(){
+    $("header .menu-icon").toggleClass('rotated');
+    $(".menu").slideToggle(400);
+    $('#contact').ScrollTo();
+  });
+  
+  $('#title').click(function(){
+    $('#top').ScrollTo();
+  });
+  
 });
