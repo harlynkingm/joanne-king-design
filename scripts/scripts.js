945981c5ae5b382657db9364b6d56d420bb77a00
Max Harlynking <mharlynk@andrew.cmu.edu>
2016-05-18 01:42:10 -0400

    changed carousel, did experiments, made site responsive

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,17 +4,44 @@ jQuery(document).ready(function(){
     $(".menu").slideToggle(400);
   });
   
-  var $grid = $('.grid').imagesLoaded( function() {
-  // init Packery after all images have loaded
-  $('.grid').packery({
-    itemSelector: '.grid-item',
-    gutter: 0
+//  var $grid = $('.grid').imagesLoaded( function() {
+//  // init Packery after all images have loaded
+//  $('.grid').packery({
+//    itemSelector: '.grid-item',
+//    gutter: 0
+//  });
+  
+  $('#top').slick({
+    arrows: false,
+    infinite: true,
+    slidesToShow: 3,
+    centerMode: true,
+    initialSlide: 1,
+    variableWidth: true,
+    lazyLoad: 'progressive',
+    autoplay: true,
+    autoplaySpeed: 5000,
+    speed: 1000
   });
-});
   
-  var swaps = [['mural.png', 'piano.png'], ['bedroom.png', 'diningroom.png'], ['livingroom3.png', 'office.png'], ['landscape.png', 'livingroom2.png']];
+//  $('.gallery img').click(function(){
+//    var obj = this;
+//    $(obj).css('transform', 'rotateY(90deg)');
+//    $(obj).css('transition', '.4s');
+//    window.setTimeout(function(){
+//      temp = obj.src;
+//      obj.src = obj.dataset.swap;
+//      obj.dataset.swap = temp;
+//      $(obj).css('transform', '');
+//    }, 400);
+//    window.setTimeout(function(){
+//      $(obj).css('transition', '.2s');
+//    }, 800);
+//  });
   
-  var refresh = setInterval(swapIn, 5000);
+  var swaps = [['diningroom.png', 'office2.png', 'livingroom2.png'], ['IMG_3084.jpg', 'IMG_1863.jpg', 'IMG_3095.jpg'], ['livingroom1.png', 'landscape.png', 'bedroom.png']];
+  
+//  var refresh = setInterval(swapIn, 5000);
   
   function swapIn(){
     var rand = Math.floor((Math.random() * swaps.length));
@@ -23,6 +50,9 @@ jQuery(document).ready(function(){
         swap(this, swaps[rand][1]);
       }
       else if (this.src.split('/').pop() == swaps[rand][1]){
+        swap(this, swaps[rand][2]);
+      }
+      else if (this.src.split('/').pop() == swaps[rand][2]){
         swap(this, swaps[rand][0]);
       }
     });
