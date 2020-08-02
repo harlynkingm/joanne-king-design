7e445090efb5849ed43f84e55b31cfcb76165324
Max Harlynking <mharlynk@andrew.cmu.edu>
2020-08-02 15:53:04 -0400

    Revert "Removed js from page"
    This reverts commit afec37650b766341c719ccd68065ec875335e564.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,38 @@
 jQuery(document).ready(function(){
-
+//  var os = new OnScreen({
+//    tolerance: 300,
+//    debounce: 0
+//  });
+//  
+//  os.on('enter', '.image-shade', (element, event) => {
+//    element.style.opacity = '0.2';
+//  });
+//  
+//  os.on('leave', '.image-shade', (element, event) => {
+//    element.style.opacity = '0.6';
+//  });
+//  
+//  $(window).scroll(() => {
+//    $(".image-shade").each( (index, elem) => {
+//      if (!isScrolledIntoView(elem)) {
+//        elem.style.opacity = '0.2';
+//      } else {
+//        elem.style.opacity = '0.6';
+//      }
+//    });
+//  });
+//  
+//  function isScrolledIntoView(elem) {
+//    var win = $(window);
+//    var viewport = {
+//        top : win.scrollTop(),
+//        left : win.scrollLeft()
+//    };
+//    viewport.right = viewport.left + win.width();
+//    viewport.bottom = viewport.top + win.height();
+//    var bounds = elem.offset();
+//    bounds.right = bounds.left + elem.outerWidth();
+//    bounds.bottom = bounds.top + elem.outerHeight();
+//    return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+//  }
 });
